0642a9348ea512f521b237667df52a67f8dae4b3
Eduarda Carvalho <eduardacarvalho9833@gmail.com>
2022-08-01 14:59:02 -0300

    feat: Modify the quantity of an item

--- js/main.js
+++ js/main.js
@@ -1,33 +1,51 @@
 const form = document.getElementById('novoItem');
 const list = document.getElementById('lista');
-const itens = [];
-function addElement(name, amount) {
+const itens = JSON.parse(localStorage.getItem('itens')) || [];
+
+function addElement(item) {
   const newItem = document.createElement('li');
   newItem.classList.add('item');
 
   const numberItem = document.createElement('strong');
-  numberItem.innerHTML = amount;
-
+  numberItem.innerHTML = item.amount;
+  numberItem.dataset.id = item.id;
   newItem.appendChild(numberItem);
-  newItem.innerHTML += name;
+  newItem.innerHTML += item.name;
 
   list.appendChild(newItem);
-
-  const itemCurrent = {
-    name,
-    amount,
-  };
-  itens.push(itemCurrent);
-  localStorage.setItem('item', JSON.stringify(itens));
 }
+
+// recarregar a pagina e continuar a lista
+itens.forEach((element) => {
+  addElement(element);
+});
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 
   const name = e.target.elements.nome;
   const amount = e.target.elements.quantidade;
+  const itemCurrent = {
+    name: name.value,
+    amount: amount.value,
+  };
+  const exists = itens.find((element) => element.name === name.value);
 
-  addElement(name.value, amount.value);
+  function updateElement(item) {
+    // eslint-disable-next-line prefer-template
+    document.querySelector("[data-id='" + item.id + "']").innerHTML = item.amount;
+  }
+
+  if (exists) {
+    itemCurrent.id = exists.id;
+    updateElement(itemCurrent);
+  } else {
+    itemCurrent.id = itens.length;
+    addElement(itemCurrent);
+    itens.push(itemCurrent);
+  }
+
+  localStorage.setItem('itens', JSON.stringify(itens));
 
   name.value = '';
   amount.value = '';
